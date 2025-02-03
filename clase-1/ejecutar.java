a37482fc9f8c4d28ba85b5ee0fff3456f2b51f39
carlos-andrex <candresfigueroa@estudiante.uniajc.edu.co>
2025-02-03 21:07:48 +0000

    se agrego el uniajc al mensaje

--- clase-1/ejecutar.java
+++ clase-1/ejecutar.java
@@ -1,7 +1,7 @@
 public class ejecutar {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!!");
+        System.out.println("Hola Mundo!!! uniajc");
         
     }
 }
